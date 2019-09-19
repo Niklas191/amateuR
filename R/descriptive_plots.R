4155f3d08e9c76e29cebfd981ec930d82b92c023
Niklas191 <Niklas-Hagemann@web.de>
2019-09-19 12:02:29 +0200

    Comments for plot code

--- R/descriptive_plots.R
+++ R/descriptive_plots.R
@@ -21,18 +21,24 @@
 #' @export
 
 plot_goal_difference <- function(game_data, team, cumulated = TRUE) {
+  # Test if team name is correct and if there are enough data points for a plot
   plot_test(game_data = game_data, team = team)
 
+  # Filter for team name
   data <- game_data$past_games %>% dplyr::filter(Home == team | Away == team)
 
+  # Compute goal diff
   data$diff <- 0
   data$diff[data$Home == team] <- data$Goals_Home[data$Home == team] - data$Goals_Away[data$Home == team]
   data$diff[data$Away == team] <- data$Goals_Away[data$Away == team] - data$Goals_Home[data$Away == team]
 
+  # Cumulate goal diff
   data$cumdiff <- cumsum(data$diff)
 
+  # Number of game day
   data$game <- seq(1, dim(data)[1], 1)
 
+  # Create plot for cumulated == FALSE
   if (cumulated == FALSE) {
     plot(x = seq(1, max(data$game), length.out = 2), y = seq(min(data$diff), max(data$diff), length.out = 2), main = paste("Goal differences for", team), xlab = "Gameday", ylab = "Goal difference", type = "n")
     abline(h = 0, col = "grey")
@@ -42,6 +48,7 @@ plot_goal_difference <- function(game_data, team, cumulated = TRUE) {
     points(x = data$game[data$diff == 0], y = data$diff[data$diff == 0], pch = 19, col = "orange", bg = "orange")
   }
 
+  # Create plot for cumulated == TRUE
   if (cumulated == TRUE) {
     plot(x = seq(1, max(data$game), length.out = 2), y = seq(min(data$cumdiff), max(data$cumdiff), length.out = 2), main = paste("Cumulated goal difference for", team), xlab = "Gameday", ylab = "Cumulated goal difference", type = "n")
     abline(h = 0, col = "grey")
@@ -73,29 +80,24 @@ plot_goal_difference <- function(game_data, team, cumulated = TRUE) {
 #' @export
 
 plot_points <- function(game_data, team) {
+  # Test if team name is correct and if there are enough data points for a plot
   plot_test(game_data = game_data, team = team)
 
+  # Filter for team name
   data <- game_data$past_games %>% dplyr::filter(Home == team | Away == team)
 
-  data$Goals_Home <- as.numeric(as.character(data$Goals_Home))
-  data$Goals_Away <- as.numeric(as.character(data$Goals_Away))
-
+  # Compute points
   data$points <- 0
   data$points[data$Home == team & (data$Goals_Home > data$Goals_Away)] <- 3
   data$points[data$Away == team & (data$Goals_Home < data$Goals_Away)] <- 3
   data$points[data$Goals_Home == data$Goals_Away] <- 1
 
+  # Cumulate points
   data$cum_points <- cumsum(data$points)
   data$game <- seq(1, dim(data)[1], 1)
 
-  plot(
-    x = seq(1, max(data$game), length.out = 2),
-    y = seq(0, max(data$cum_points), length.out = 2),
-    main = paste("Cumulated points for", team, "over time"),
-    xlab = "Gameday",
-    ylab = "Cumulated points",
-    type = "n"
-  )
+  # Create plot
+  plot(x = seq(1, max(data$game), length.out = 2), y = seq(0, max(data$cum_points), length.out = 2), main = paste("Cumulated points for", team, "over time"), xlab = "Gameday", ylab = "Cumulated points", type = "n")
   points(x = data$game, y = data$cum_points, col = "black", type = "l")
   points(x = data$game[data$points == 3], y = data$cum_points[data$points == 3], pch = 19, col = "green", bg = "green")
   points(x = data$game[data$points == 1], y = data$cum_points[data$points == 1], pch = 19, col = "orange", bg = "orange")
