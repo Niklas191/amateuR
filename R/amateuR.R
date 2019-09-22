91b1be0e18161b5f319ff9310a8bed860ce52480
James Travis <james.travis@stud.uni-goettingen.de>
2019-09-22 17:59:05 +0200

    Proofreading

--- R/amateuR.R
+++ R/amateuR.R
@@ -1,8 +1,8 @@
 #' Prediction for amateur German football outcomes
 #'
 #' amateuR is an R Package for obtaining data for amateur German football leagues and predicting future matches with a Dixon Coles Model.
-#' @details The package provides functions to download German amateur football data from kicker.de by using web scraping methods and the rvest package.
-#' Based on the downloaded data the table, individual game results and the date and time of future games can be shown.
+#' @details The package provides functions to obtain German amateur football data from kicker.de by using web scraping methods and the rvest package.
+#' Based on this data, the table, individual game results and the date and time of future games can be shown.
 #' It also provides a function for predicting future games.
 #' Some of the results can visualised using specific plot functions.
 #' @author James Travis <james.travis@@stud.uni-goettingen.de>
